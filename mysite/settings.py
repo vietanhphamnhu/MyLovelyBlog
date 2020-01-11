b92b0bef8536bc17cc98920af6f3af6434d6e6e7
vietanhphamnhu <40441000+vietanhphamnhu@users.noreply.github.com>
2020-01-12 02:25:15 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   os.path.join(BASE_DIR, 'static'),
 ]
 
 
